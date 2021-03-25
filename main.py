1be3251be0141e5bb085c9cd4ff6a8ece8d895ab
Derek Caramella <derekcaramella@gmail.com>
2021-03-25 12:40:55 -0400

    Build

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Import necessary modules.
 import docx2txt
+import pandas as pd
 import os
 
+df_list = []  # Prepare empty list for export dataframe
 for dieline in os.listdir('DS Smith Dielines'):  # Loop through the directory, which analyzes each dieline
     text = docx2txt.process('DS Smith Dielines/' + dieline)  # Routes directory & scraps text
     text = text.replace('\n', ',')  # Removes all alt-enters from string
@@ -18,5 +20,35 @@ for dieline in os.listdir('DS Smith Dielines'):  # Loop through the directory, w
 
     text = text.replace(' ', '')  # Remove all spaces to narrow string
     text = text.replace('	', '')  # Remove all tabs to narrow string
-
-    print(text)  # Return comprehensive string for splicing
+    list_prompts = text.split(',')  # Delimit by comma, this will pair parameter & process value
+    design, description, style, board = '', '', '', ''  # Nullify sample for dataframe
+    inner_dimension, finished_piece_weight, over_all_blank, combination = '', '', '', ''  # Nullify sample for dataframe
+    for instance in list_prompts:  # Within one file, loop through ever instance
+        if 'Design#:' in instance:  # If Design#: exists, capture process value
+            design = instance.partition('Design#:')[2]  # Bifurcate string & return process value
+        if 'Description:' in instance:  # If Description: exists, capture process value
+            # Sometimes the description was comma splice, so grab current & next instance
+            description = instance.partition('Description:')[2] + str(list_prompts[list_prompts.index(instance) + 1])
+        if 'Style:' in instance:  # If Style: exists, capture process value
+            style = instance.partition('Style:')[2]  # Bifurcate string & return process value
+        if 'Board:' in instance:  # If Board: exists, capture process value
+            board = instance.partition('Board:')[2]  # Bifurcate string & return process value
+        if 'I.D.:' in instance:  # If I.D.: exists, capture process value
+            inner_dimension = instance.partition('I.D.:')[2]  # Bifurcate string & return process value
+        if 'FinishedPieceWeight:' in instance:  # If Finished Piece Weight: exists, capture process value
+            finished_piece_weight = instance.partition('FinishedPieceWeight:')[2]
+        if 'Over-allBlank:' in instance:  # If Over-all Blank: exists, capture process value
+            over_all_blank = instance.partition('Over-allBlank:')[2]  # Bifurcate string & return process value
+        if 'Combination:' in instance:  # If Combination: exists, capture process value
+            combination = instance.partition('Combination:')[2]  # Bifurcate string & return process value
+    # Following one file, compile results into one tuple
+    sample = (dieline, design, description, style, board, inner_dimension,
+              finished_piece_weight, over_all_blank, combination)
+    # Append sample into the staging list for dataframe
+    df_list.append(sample)
+# Generate dataframe from list of tuples
+export_df = pd.DataFrame.from_records(df_list, columns=['File', 'Design', 'Description', 'Style', 'Board',
+                                                        'Inner Dimension', 'Finished Piece Weight',
+                                                        'Over-all Blank', 'Combination'])
+# Export dataframe
+export_df.to_excel('DS Smith Specifications.xlsx', index=False)
