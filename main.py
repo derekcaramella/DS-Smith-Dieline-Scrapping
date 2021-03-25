87e17e5601761de00055383f3183e3302baa1231
Derek Caramella <derekcaramella@gmail.com>
2021-03-25 07:19:09 -0400

    Base level algorithm development to remove noise, the packaging engineer will provide insight on what is important.

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+# Import necessary modules.
+import docx2txt
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+for dieline in os.listdir('DS Smith Dielines'):  # Loop through the directory, which analyzes each dieline
+    text = docx2txt.process('DS Smith Dielines/' + dieline)  # Routes directory & scraps text
+    text = text.replace('\n', ',')  # Removes all alt-enters from string
+    # Goal: Remove sequence of commas, but leave one comma for splicing
+    for index in range(0, len(text)):  # For each index in the string
+        if text[index] == ',' and text[index + 1] == ',':  # If the current index is a comma & the next is a comma
+            text = text[:index] + ' ' + text[index + 1:]  # Maintain string, but replace with a space to maintain length
 
+    text = text.replace(' ', '')  # Remove all spaces to narrow string
+    # Goal: Remove :, instances for string splicing.
+    for index in range(0, len(text)):  # For each index in the string
+        if text[index] == ',' and text[index - 1] == ':':  # If the current index is a comma & the previous is a colon
+            text = text[:index] + ' ' + text[index + 1:]  # Maintain string, but replace with a space to maintain length
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    text = text.replace(' ', '')  # Remove all spaces to narrow string
+    text = text.replace('	', '')  # Remove all tabs to narrow string
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(text)  # Return comprehensive string for splicing
